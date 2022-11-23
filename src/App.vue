318145abf1e88b0767b1252b2102a233a339c847
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-11-24 00:04:02 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import PageTitle from './components/PageTitle.vue';
+export default {
+  components: {
+    PageTitle
+  }
+}
+</script>
+
+<template>
+  <div>
+    <PageTitle/>
+  </div>
+</template>
+
+<style scoped>
+</style>
